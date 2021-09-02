0b95677259b77035fe23d2015e00521b582e0adb
Karol <mouracinthia1@gmail.com>
2021-09-02 15:46:20 -0300

    [Feature Cinthia - Task: Finalização Controller do Usuario]

--- projetoIntegrador/src/main/java/com/baependi/projetoIntegrador/controller/ControllerUsuario.java
+++ projetoIntegrador/src/main/java/com/baependi/projetoIntegrador/controller/ControllerUsuario.java
@@ -3,10 +3,16 @@ package com.baependi.projetoIntegrador.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,4 +49,18 @@ public class ControllerUsuario  {
 	    	return ResponseEntity.status(200).body(objetoUsuario);
 	    }
 	}
+	@PostMapping("/salvar")
+	private ResponseEntity <Usuario> salvarUsuario(@Valid @RequestBody Usuario novoUsuario){
+		return ResponseEntity.status(201).body(repositorio.save(novoUsuario));
+	}
+	@PutMapping("/atualizar")
+	private ResponseEntity <Usuario> atualizarUsuario(@Valid @RequestBody Usuario atualizarUsuario){
+		return ResponseEntity.status(201).body(repositorio.save(atualizarUsuario));
+		
+	}
+	@DeleteMapping("/deletar/{id}")
+	private void deletarUsuario(@PathVariable(value = "id") Long idUsuario) {
+		repositorio.deleteById(idUsuario);
+	}
 }
+  
