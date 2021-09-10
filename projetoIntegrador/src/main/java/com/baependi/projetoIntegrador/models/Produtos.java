93403c5baa2cf059e4c9c095ac0ed356a18887ae
Karol <mouracinthia1@gmail.com>
2021-09-10 11:03:49 -0300

    [Feature Cinthia -Task: UsuarioDto implementado]

--- projetoIntegrador/src/main/java/com/baependi/projetoIntegrador/models/Produtos.java
+++ projetoIntegrador/src/main/java/com/baependi/projetoIntegrador/models/Produtos.java
@@ -10,13 +10,11 @@ import javax.validation.constraints.Size;
 
 
 import com.sun.istack.NotNull;
-import lombok.Getter;
-import lombok.Setter;
+
 
 @Entity
 @Table(name = "tb_produtos")
-@Getter
-@Setter
+
 public class Produtos {
 
 	private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long idProduto;
@@ -25,6 +23,42 @@ public class Produtos {
 	private @NotBlank String descricaoDoProduto;
 	private @NotBlank String autoreDoProduto;
 	private @NotBlank @Size(min = 1, max = 75) String tipoDeProduto;
+	public Long getIdProduto() {
+		return idProduto;
+	}
+	public void setIdProduto(Long idProduto) {
+		this.idProduto = idProduto;
+	}
+	public String getNomeDoProduto() {
+		return nomeDoProduto;
+	}
+	public void setNomeDoProduto(String nomeDoProduto) {
+		this.nomeDoProduto = nomeDoProduto;
+	}
+	public float getPrecoDoProduto() {
+		return precoDoProduto;
+	}
+	public void setPrecoDoProduto(float precoDoProduto) {
+		this.precoDoProduto = precoDoProduto;
+	}
+	public String getDescricaoDoProduto() {
+		return descricaoDoProduto;
+	}
+	public void setDescricaoDoProduto(String descricaoDoProduto) {
+		this.descricaoDoProduto = descricaoDoProduto;
+	}
+	public String getAutoreDoProduto() {
+		return autoreDoProduto;
+	}
+	public void setAutoreDoProduto(String autoreDoProduto) {
+		this.autoreDoProduto = autoreDoProduto;
+	}
+	public String getTipoDeProduto() {
+		return tipoDeProduto;
+	}
+	public void setTipoDeProduto(String tipoDeProduto) {
+		this.tipoDeProduto = tipoDeProduto;
+	}
 
 	/*@OneToMany(orphanRemoval = true)
 	@JoinColumn(name = "produtos_id_produto")
