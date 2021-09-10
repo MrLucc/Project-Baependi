f7811aa8c3d494e15532e423bb9c9175a19b02b8
sandrosilvalima <sandrosilvalima6@gmail.com>
2021-09-10 12:29:35 -0300

    [Feature Sandro - Task: Inclusão do package service --> ServiceUsuario

--- projetoIntegrador/src/main/java/com/baependi/projetoIntegrador/models/Produtos.java
+++ projetoIntegrador/src/main/java/com/baependi/projetoIntegrador/models/Produtos.java
@@ -1,13 +1,13 @@
 package com.baependi.projetoIntegrador.models;
 
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-
 
 import com.sun.istack.NotNull;
 
