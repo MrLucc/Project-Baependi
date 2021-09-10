bd2240a289784cf17ae67129fb9841703a88dac9
Lucas <silvalucs212@gmail.com>
2021-09-10 15:26:45 -0300

    [Feature Lucas - Task: adição da notação @Service na class UserDetailsServiceImplements]

--- projetoIntegrador/src/main/java/com/baependi/projetoIntegrador/security/UserDetailsServiceImplements.java
+++ projetoIntegrador/src/main/java/com/baependi/projetoIntegrador/security/UserDetailsServiceImplements.java
@@ -9,19 +9,21 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import com.baependi.projetoIntegrador.models.Usuario;
 import com.baependi.projetoIntegrador.repository.RepositorioUsuario;
-import com.baependi.projetoIntegrador.security.UserDetailsImplements;
+import org.springframework.stereotype.Service;
 
-public class UserDetailsServiceImplements implements UserDetailsService{
+@Service
+public class UserDetailsServiceImplements implements UserDetailsService {
 
-	private @Autowired RepositorioUsuario repository;
+    private @Autowired
+    RepositorioUsuario repository;
 
-	@Override 
-	public UserDetails loadUserByUsername (String nomeUsuario) throws UsernameNotFoundException{
-		Optional<Usuario> objetoOptional = repository.findByEmail(nomeUsuario);
-				if(objetoOptional.isPresent()) {
-					return new UserDetailsImplements(objetoOptional.get());
-				}else {
-					throw new UsernameNotFoundException(nomeUsuario + "Não existe");
-				}
-	}
+    @Override
+    public UserDetails loadUserByUsername(String nomeUsuario) throws UsernameNotFoundException {
+        Optional<Usuario> objetoOptional = repository.findByEmail(nomeUsuario);
+        if (objetoOptional.isPresent()) {
+            return new UserDetailsImplements(objetoOptional.get());
+        } else {
+            throw new UsernameNotFoundException(nomeUsuario + "Não existe");
+        }
+    }
 }
