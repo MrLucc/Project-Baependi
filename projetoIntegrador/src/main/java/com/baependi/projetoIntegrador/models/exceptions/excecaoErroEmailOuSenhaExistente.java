1592e9439819228fec25a58612ee1e3a24b8d892
Julio Teixeira <julioteixeirapimont@gmail.com>
2021-09-21 00:29:06 -0300

    Atualização de Folders

--- projetoIntegrador/src/main/java/com/baependi/projetoIntegrador/models/exceptions/excecaoErroEmailOuSenhaExistente.java
+++ projetoIntegrador/src/main/java/com/baependi/projetoIntegrador/models/exceptions/excecaoErroEmailOuSenhaExistente.java
@@ -3,7 +3,7 @@ package com.baependi.projetoIntegrador.models.exceptions;
 public class excecaoErroEmailOuSenhaExistente extends RuntimeException {
 	/*
 	 * 
-	 * 
+	 * a
 	 */
 	private static final long SerialVersionUid = 1L;
 
