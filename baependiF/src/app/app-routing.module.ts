ab2d83cf88d76b3511c5ea3b5b4a711fe16c9808
Cristiano-Silva-Soares <criztianosoares99@outlook.com>
2021-09-29 22:37:24 -0300

    [Feature Cristiano - Task: Implementação de nav bar e rotas entre dependencias do site]

--- baependiF/src/app/app-routing.module.ts
+++ baependiF/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContatoComponent } from './contato/contato.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { MenuComponent } from './menu/menu.component';
 import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 
 const routes: Routes = [
@@ -9,7 +10,8 @@ const routes: Routes = [
 
   {path:'inicio', component: InicioComponent},
   {path:'contato', component: ContatoComponent},
-  {path:'sobre-nos', component: SobreNosComponent}
+  {path:'sobre-nos', component: SobreNosComponent},
+  {path: 'menu', component: MenuComponent}
 ];
 
 @NgModule({
