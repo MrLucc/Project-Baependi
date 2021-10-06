8e17e1d40c149ce43ef1191fa602cfcf882d62e6
Cristiano-Silva-Soares <criztianosoares99@outlook.com>
2021-10-05 22:29:14 -0300

    [Feature Cristiano - Task: Atualização nas dependências da landing page]

--- baependiF/src/app/app.component.ts
+++ baependiF/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AutService } from './service/aut.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Baependi';
+
+  constructor(
+    public aut: AutService
+  ){}
 }
